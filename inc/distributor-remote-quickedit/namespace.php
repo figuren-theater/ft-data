4011d6c4d76b81dbf4547f132023978821454b00
carstenbach <mail@carsten-bach.de>
2023-02-16 12:29:12 +0100

    Fix composer source of 'Distributor - Remote Qickedit' Plugin, as it is now avail. via wpackagist

--- inc/distributor-remote-quickedit/namespace.php
+++ inc/distributor-remote-quickedit/namespace.php
@@ -18,7 +18,7 @@ use function is_network_admin;
 use function is_user_admin;
 
 const BASENAME   = 'distributor-remote-quickedit/distributor-remote-quickedit.php';
-const PLUGINPATH = FT_VENDOR_DIR . '/carstingaxion/' . BASENAME;
+const PLUGINPATH = FT_VENDOR_DIR . '/wpackagist-plugin/' . BASENAME;
 
 /**
  * Bootstrap module, when enabled.
