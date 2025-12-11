287a96db0271561d19d7283c5a56538818814718
carstingaxion <carstingaxion@users.noreply.github.com>
2025-12-11 11:42:42 +0000

    Update version-numbers to 1.0.35

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://figuren.theater
  * Text Domain:     figurentheater
  * Domain Path:     /languages
- * Version:         1.0.34
+ * Version:         1.0.35
  *
  * @package         figuren-theater/ft-data
  */
